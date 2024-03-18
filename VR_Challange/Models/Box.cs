4d0c3901117172fc52240ddc6c590d5cb8419713
palina.sh <palina.sh@gmail.com>
2024-03-18 12:22:41 +0300

    Project has been added

---
+++ VR_Challange/Models/Box.cs
@@ -0,0 +1,11 @@
+﻿namespace VR_Challange.Models
+{
+    public sealed class Box
+    {
+        public string SupplierIdentifier { get; set; }
+
+        public string Identifier { get; set; }
+
+        public IReadOnlyCollection<Content> Contents { get; set; }
+    }
+}
---
+++ VR_Challange/Models/Content.cs
@@ -0,0 +1,11 @@
+﻿namespace VR_Challange.Models
+{
+    public class Content
+    {
+        public string PoNumber { get; set; }
+
+        public string Isbn { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
