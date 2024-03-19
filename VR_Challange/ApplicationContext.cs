6b1179ba576ef51b455c017cb60a9f267b67ea45
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2024-03-19 17:08:20 +0200

    DBContext for the application has been added

---
+++ VR_Challange/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VR_Challange.Models;
+
+namespace VR_Challange
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Box> Boxes => Set<Box>();
+
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=VR_Challange.db");
+        }
+    }
+}
